af150617e3d2e367e418d554cf8f5a1393296469
Douglas Kosvoski <douglas.contactpro@gmail.com>
2019-07-05 12:24:43 -0300

    idented equal signs

--- SCRIPTS/BUTTOMS.gd
+++ SCRIPTS/BUTTOMS.gd
@@ -7,12 +7,12 @@ func _ready():
 	$"CREDITS"
 	
 	$"QUIT"
-	
+
 func _physics_process(delta):
 	# MOUSE ON BUTTOMS
-	if $"START".is_hovered() == true: $"START".grab_focus()
+	if $"START".is_hovered()     == true: $"START".grab_focus()
 	elif $"CREDITS".is_hovered() == true: $"CREDITS".grab_focus()
-	elif $"QUIT".is_hovered() == true: $"QUIT".grab_focus()
+	elif $"QUIT".is_hovered()    == true: $"QUIT".grab_focus()
 
 
 func _on_START_pressed():
