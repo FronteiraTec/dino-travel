f344c46aa077b73d7e1490d154532736c76c24fc
Douglas Kosvoski <douglas.contactpro@gmail.com>
2019-07-05 12:26:29 -0300

    changed buttom functions... now levels 1 and 2 are accessible

--- SCRIPTS/LEVEL_SELECTION.gd
+++ SCRIPTS/LEVEL_SELECTION.gd
@@ -1,16 +1,9 @@
 extends Control
 
 
-func _ready():
-	pass
-
-
-func _physics_process(delta):
-	pass
-
-
 func _on_VOLTAR_pressed():
 	get_tree().change_scene('res://SCENES/UI/MENU.tscn')
+
 
 func _on_Button_pressed():
 	get_tree().change_scene('res://SCENES/LEVELS/LEVEL_01.tscn')
@@ -19,10 +12,10 @@ func _on_Button2_pressed():
 	get_tree().change_scene('res://SCENES/LEVELS/LEVEL_02.tscn')
 
 func _on_Button3_pressed():
-	get_tree().change_scene('res://SCENES/UI/LEVELSELECTION.tscn')
+	get_tree().change_scene('res://SCENES/UI/LEVEL_SELECTION.tscn')
 
 func _on_Button4_pressed():
-	get_tree().change_scene('res://SCENES/UI/LEVELSELECTION.tscn')
+	get_tree().change_scene('res://SCENES/UI/LEVEL_SELECTION.tscn')
 
 func _on_Button5_pressed():
-	get_tree().change_scene('res://SCENES/UI/LEVELSELECTION.tscn')
+	get_tree().change_scene('res://SCENES/UI/LEVEL_SELECTION.tscn')
