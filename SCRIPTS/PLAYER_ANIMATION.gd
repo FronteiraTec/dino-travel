5ac01e2904c46d6c9af31719327e047839617b74
Douglas Kosvoski <douglas.contactpro@gmail.com>
2019-08-26 11:41:14 -0300

    added some future functions

--- SCRIPTS/PLAYER_ANIMATION.gd
+++ SCRIPTS/PLAYER_ANIMATION.gd
@@ -2,11 +2,24 @@ extends AnimationPlayer
 
 func _ready():
 	var anim = get_node(".").get_animation("default")
-
-func _process(delta):	
-	# play animation
-	if Input.is_action_just_pressed('ui_forward'):
-		get_node(".").play("default")
-	
 	# set animation speed
 	get_node(".").set_speed_scale(1)
+	
+	
+func _process(delta):	
+	# run animation
+	if Input.is_action_just_pressed('ui_forward'):
+		get_node(".").play("default")
+		
+#	PLAY ROTATION ANIMATION // ANIMATION NEED TO BE CREATED
+#	elif Input.is_action_just_pressed('ui_left'):
+#		get_node(".").play("rotation")
+
+		
+# 	PLAY ROTATION ANIMATION // ANIMATION NEED TO BE CREATED
+#	elif Input.is_action_just_pressed('ui_right'):
+#		get_node(".").play("rotation")
+
+#	else:
+#		get_node(".").play('idle')
+
