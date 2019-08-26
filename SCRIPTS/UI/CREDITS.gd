a22aa54da49e0d38c7626dd9eafbb34def14a140
Douglas Kosvoski <douglas.contactpro@gmail.com>
2019-08-26 15:43:42 -0300

    changed velocity

--- SCRIPTS/UI/CREDITS.gd
+++ SCRIPTS/UI/CREDITS.gd
@@ -1,10 +1,10 @@
 extends KinematicBody2D
 
 # Apply movement to credits text
-export var up_speed = 100
+export var up_speed = 2.5
 
 func _ready():
 	pass
 
 func _process(delta):
-	position.y -= up_speed * delta
+	position.y -= up_speed
